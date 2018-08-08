ec02e7c3eb6f3114389aa9c8426db3793dab479f
Adam <adamqkc@gmail.com>
2018-08-08 16:40:09 -0700

    add ejs files and main app.js file

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -8,6 +11,10 @@ var urlDatabase = {
 "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
@@ -22,6 +29,12 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);ç
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
