b9b05f5b4595ac4c4f9562ce3fa6fde0d326fc75
ADAM CHEN <ADAMQKC@GMAIL.COM>
2018-08-09 00:47:54 -0700

    add generateRandomString function

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+var main = require('./main');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -22,19 +23,21 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  let templateVars = 
-  { shortURL: req.params.id,
+  let templateVars = { 
+    shortURL: req.params.id,
     urls: urlDatabase
   };
   res.render("urls_show", templateVars);ç
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  
+  Object.assign(urlDatabase, req.body);
+  console.log(req.body);
+  console.log(urlDatabase);
+  res.send("Ok");         
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
---
+++ main.js
@@ -0,0 +1,11 @@
+function generateRandomString() {
+  var shortURL = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 5; i++)
+    shortURL += characters.charAt(Math.floor(Math.random() * characters.length));
+
+  return shortURL;
+}
+
+exports.generateRandomString = generateRandomString;
